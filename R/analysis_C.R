0978aaa5f92b07889b42a156c1b85bf884d643e2
catrinehom <48596773+catrinehom@users.noreply.github.com>
2020-04-14 11:32:04 +0200

    add analysis_C.R

---
+++ R/analysis_C.R
@@ -0,0 +1,40 @@
+# Clear workspace
+# ------------------------------------------------------------------------------
+rm(list = ls())
+
+# Load libraries
+# ------------------------------------------------------------------------------
+library("tidyverse")
+
+# Define functions
+# ------------------------------------------------------------------------------
+source(file = "R/99_project_functions.R")
+
+# Load data
+# ------------------------------------------------------------------------------
+clincal_data_aug <- read_csv(file = "data/02_clincal_data_clean.csv")
+PAM50_aug <- read_csv(file = "data/02_PAM50_clean.csv")
+proteome_data_aug <- read_csv(file = "data/02_proteome_data_clean.csv")
+
+# Wrangle data
+# ------------------------------------------------------------------------------
+my_data_clean_aug %>% ...
+
+# Model data
+# ------------------------------------------------------------------------------
+my_data_clean_aug %>% ...
+
+# Visualise data
+# ------------------------------------------------------------------------------
+ggplot(data = clincal_data_aug) +
+  geom_histogram(mapping = aes(x = `Age at Initial Pathologic Diagnosis`), binwidth = 5)
+
+ggplot(data = clincal_data_aug, mapping = aes(x = x, y = y)) + 
+  geom_point()
+#> Warning: Removed 9 rows containing missing values (geom_point).
+
+# Write data
+# ------------------------------------------------------------------------------
+write_tsv(x = my_data_clean_aug,
+          path = "data/04_my_data_clean_aug_anl.tsv")
+ggsave(...)
