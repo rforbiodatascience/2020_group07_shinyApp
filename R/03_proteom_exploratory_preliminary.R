940343a32c28b15bbd73ea9ddb24f2c00feeb6e2
zidibot <gvladja@gmail.com>
2020-04-19 20:26:09 +0000

    "some exploratory analysis"

--- R/03_proteom_exploratory_preliminary.R
+++ R/03_proteom_exploratory_preliminary.R
@@ -13,8 +13,11 @@ library("pheatmap")
 
 # Load data
 # ------------------------------------------------------------------------------
+# 
+# ------------------------------------------------------------------------------
 df <- read_csv(file = "data/02_joined_data_clean.csv")
 
+# ------------------------------------------------------------------------------
 proteom_mtx <- df %>% 
   column_to_rownames(., "patient_ID") %>%
   select(starts_with("NP_"),
@@ -29,11 +32,34 @@ annotation <- df %>% column_to_rownames(., "patient_ID") %>%
   select(PAM50_mRNA) 
 
 
+png(filename = "results/heatmap_tissueAnnotation.png")
+mtx %>%  pheatmap(.,
+                  clustering_distance_rows = "correlation",
+                  clustering_distance_cols = "correlation",
+                  annotation = annotation,angle_col = 45)
+dev.off()
 
-mtx %>%
-  pheatmap(.,
-           clustering_distance_rows = "correlation",
-           clustering_distance_cols = "correlation",
-           annotation = annotation,angle_col = 45)
+# ------------------------------------------------------------------------------
+df %>%   select(patient_ID,
+                starts_with("NP_"),
+                PAM50_mRNA) %>%
+  pivot_longer(cols = starts_with('NP_')) %>%
+  ggplot(aes(x=value, fill=patient_ID)) + geom_density(alpha=0.5, bins=100) + 
+  geom_vline(xintercept = c(-0.58, 0.58), linetype="dashed") + 
+  ggtitle("") +
+  theme_bw(base_family = "Times", base_size = 10) +
+  theme(legend.position = "none")
+ggsave(filename = "results/density_sample_expression.png",device = "png")
 
 
+# ------------------------------------------------------------------------------
+df %>%   select(patient_ID,
+                starts_with("NP_"),
+                PAM50_mRNA) %>%
+  pivot_longer(cols = starts_with('NP_')) %>%
+  ggplot(aes(x=value, fill=PAM50_mRNA)) + geom_density(alpha=0.5, bins=100) + 
+  geom_vline(xintercept = c(-0.58, 0.58), linetype="dashed") + 
+  ggtitle("") +
+  theme_bw(base_family = "Times", base_size = 10) +
+  theme(legend.position = "none")
+ggsave(filename = "results/density_tissue_expression.png",device = "png")
