bcb810011a10270ab6857f5e42d0c55f4ee468c5
catrinehom <catrinehom@gmail.com>
2020-04-25 12:47:55 +0200

    update doit

--- R/00_doit.R
+++ R/00_doit.R
@@ -1,7 +1,7 @@
 # Run all scripts
 # ------------------------------------------------------------------------------
-source(file = "R/01_load.R")
-source(file = "R/02_clean.R")
-source(file = "R/03_augment.R")
-source(file = "R/04_analysis_i.R")
-source(file = "R/05_model_X.R")
+source(file = "R/01_load_and_clean.R.R")
+source(file = "R/02_augment.R")
+source(file = "R/03_clinical_exploratory_plots.R")
+source(file = "R/03_proteom_exploratory_preliminary.R")
+source(file = "R/05_ANN_model.R")
