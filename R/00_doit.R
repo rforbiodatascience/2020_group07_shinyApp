7dacb9c34a078478af75e2858498eb845b2d14c4
catrinehom <catrinehom@gmail.com>
2020-04-27 11:54:40 +0200

    update doit

--- R/00_doit.R
+++ R/00_doit.R
@@ -2,6 +2,6 @@
 # ------------------------------------------------------------------------------
 source(file = "R/01_load_and_clean.R.R")
 source(file = "R/02_augment.R")
-source(file = "R/03_clinical_exploratory_plots.R")
-source(file = "R/03_proteom_exploratory_preliminary.R")
+source(file = "R/03_EDA.R")
+# 04_Paulas_clustering?
 source(file = "R/05_ANN_model.R")
