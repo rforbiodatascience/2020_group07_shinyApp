8dd89905549721ecccd99bd54e497bd6e550a895
zidibot <gvladja@gmail.com>
2020-05-03 12:31:08 +0000

    Added a top_genes function for our dataset

--- R/99_project_functions.R
+++ R/99_project_functions.R
@@ -1,3 +1,10 @@
+# Load libraries
+# ------------------------------------------------------------------------------
+
+if (!requireNamespace("docstring", quietly = TRUE))
+  install.packages("docstring")
+library(docstring)
+
 # Define project functions
 # ------------------------------------------------------------------------------
 as_matrix <- function(x){
@@ -10,18 +17,31 @@ as_matrix <- function(x){
 # ------------------------------------------------------------------------------
 # Extract the plot's legend
 library(gridExtra)
-get_legend<-function(myggplot){
+get_legend <- function(myggplot){
+  #' Returns the "legend" object from a plot object:
+  #' 
+  #' @param myggplot a ggplot object
   tmp <- ggplot_gtable(ggplot_build(myggplot))
   leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
   legend <- tmp$grobs[[leg]]
   return(legend)
 }
 
+# Wrap for `?function_name``
+docstring(get_legend)
+
 # Extract top genes from PAM50_mRNA grouped data:
 # takes the filterby as astring, a new label for the column and a number of genes to return
 # ------------------------------------------------------------------------------
 # Data split for each cancer group: without CONTROL samples
 return_top_genes <- function(data, filter_by, new_label, n = 10){
+  #' Data wrangling helper function:
+  #' 
+  #' Returns the top n differentially expressed genes for a subset of data
+  #' 
+  #' @param filter_by specifying the subset string for filter()
+  #' @param new_label specify the new column name in the final tible
+  #' @param n the number of top expressed genes
   data <- data %>% 
     select(patient_ID, PAM50_mRNA, starts_with("NP_")) %>%
     filter(PAM50_mRNA == filter_by) %>%
@@ -36,18 +56,10 @@ return_top_genes <- function(data, filter_by, new_label, n = 10){
   return(data)
 }
 
-# df_big %>% 
-#   select(patient_ID, PAM50_mRNA, starts_with("NP_")) %>%
-#   filter(PAM50_mRNA == "Luminal B") %>%
-#   pivot_longer(data = ., cols = -c(PAM50_mRNA, patient_ID)) %>% 
-#   pivot_wider(data = ., values_from = value, names_from = patient_ID) %>%
-#   mutate(avg = rowMeans(select(.,-PAM50_mRNA,-name), na.rm = TRUE)) %>%
-#   mutate(avg = abs(avg)) %>%
-#   arrange(desc(avg)) %>%
-#   select(name) %>%
-#   rename(new_label = name) %>%
-#   slice(.,1:n)
-# return(data)
+# Wrap for `?function_name``
+docstring(return_top_genes)
+
+
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
