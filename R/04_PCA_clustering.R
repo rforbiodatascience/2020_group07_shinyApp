954a375fc7938638fe2f7c148232bf314f995ac5
paurrodri <paula@clinical-microbiomics.com>
2020-05-04 14:28:39 +0200

    change 04

--- R/04_PCA_clustering.R
+++ R/04_PCA_clustering.R
@@ -4,8 +4,8 @@ rm(list = ls())
 
 # Load libraries
 # ------------------------------------------------------------------------------
-library("tidyverse")
-library("broom")
+library(tidyverse)
+library(broom)
 library(patchwork)
 
 # Define functions
@@ -19,7 +19,9 @@ joined_data_aug <- read.csv(file = "data/02_joined_data_aug.csv")
 # Wrangle data
 # ------------------------------------------------------------------------------
 proteome_data <- joined_data_aug %>% 
-  select(starts_with("NP"))
+  filter(PAM50_mRNA != "Control") %>%  # remove healthy control samples
+  select(starts_with("NP"))            # select only proteome-count columns
+
 
 # PCA 
 # ---------------------------------------------------------------------------
@@ -35,14 +37,17 @@ pca %>%
   labs(title = "Scree plot - PCA proteome data", 
        x = "Principal components",
        y = "variance explained (%)") +
-  theme(plot.title = element_text(hjust = 0.5))
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme_gray() +
   scale_y_continuous(labels = scales::percent)
 ggsave(filename = "results/04_scree_plot.png",device = "png")
 
 ### Augment and add y class (PAM50_mRNA clusters)
 proteome_pca_aug <- pca %>%
   augment(proteome_data) %>%
-  mutate(class = joined_data_aug$PAM50_mRNA)
+  mutate(class = joined_data_aug$PAM50_mRNA[1:77]) %>% # add class
+  mutate(class = factor(class)) # set class as factor
+  # should we change the names of the levels? make them shorter
 
 
 ### Scatter proteome data - PC1/PC2
@@ -59,7 +64,7 @@ ggsave(filename = "results/04_PCA.png",device = "png")
 
 # K-means clustering
 # ------------------------------------------------------------------------------
-k = length(levels(proteome_pca_aug$class)) # 5 levels
+k = length(levels(proteome_pca_aug$class)) # 4 levels
 
 ### Clustering on original data
 cluster_original <- proteome_data %>%
@@ -154,3 +159,30 @@ plot3 <- proteome_pca_cluster_aug %>%
 (plot1 + plot2 + plot3)
 ggsave(filename = "results/04_PCA_kmeans.png",
        device = "png")
+
+
+# Which clustering technique performs better
+# ------------------------------------------------------------------------------
+
+proteome_pca_cluster_aug %>%
+  
+  select(class, cluster_original, cluster_pca) %>%
+  
+  mutate(cluster_original = case_when(cluster_original == 1 ~ "Basal-like",
+                                 cluster_original == 2 ~ "Luminal B",
+                                 cluster_original == 3 ~ "HER2-enriched",
+                                 cluster_original == 4 ~ "Luminal A"),
+         cluster_pca = case_when(cluster_pca == 1 ~ "HER2-enriched",
+                                 cluster_pca == 2 ~ "Luminal B",
+                                 cluster_pca == 3 ~ "Luminal A",
+                                 cluster_pca == 4 ~ "Basal-like"),
+         
+         cluster_original_correct = case_when(class == cluster_original ~ 1,
+                                         class != cluster_original ~ 0),
+         cluster_pca_correct = case_when(class == cluster_pca ~ 1,
+                                         class != cluster_pca ~ 0)) %>% 
+  
+  summarise(score_original = mean(cluster_original_correct) * 100,
+            score_pca = mean(cluster_pca_correct) * 100)
+
+# should we print the tibble?
